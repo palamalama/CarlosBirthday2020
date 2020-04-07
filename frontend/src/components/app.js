22f42d0ed25785a4b9b87a0fb8627e7460236305
palamalama <ericgemmell@gmail.com>
2020-04-07 13:37:47 +0100

    added login and main page #real basic

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,20 +1,37 @@
 import React from "react";
+import Login from "./Login/loginMain";
+import Game from "./Game/gameMain";
 
 class App extends React.Component {
 
 	constructor(props){
 		super(props);
+		this.login = this.login.bind(this);
 		this.state = {
 		};
 	}
 
 	render() {
-		return (
-			<div className="app" style={{"width":"100%","height":"100%"}}
-			>
-			Hello Home Slice
-			</div>
-		);
+		if(this.state.name != undefined){
+			return (
+				<div className="app" style={{"width":"100%","height":"100%"}}>
+					<Game name={this.state.name} />
+				</div>
+			);
+		}
+		else{
+			return (
+				<div className="app" style={{"width":"100%","height":"100%"}}>
+					<Login login={this.login}/>
+				</div>
+			);
+		}
+	}
+	login(name){
+		console.log("You just Logged In!",name);
+		this.setState({
+			name:name||"Anonymous Alpaca"
+		});	
 	}
 }
 
